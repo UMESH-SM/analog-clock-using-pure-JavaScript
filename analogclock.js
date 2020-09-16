796cfc3b2abfce9cf1649635dbbb291582aed07f
UMESH-SM <umeshsmg007@gmail.com>
2020-09-16 21:06:29 +0530

    done

---
+++ analogclock.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    
+    hrotation = htime*30 + mtime/2;
+    mrotation = mtime*6;
+    srotation = stime*6;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+})
